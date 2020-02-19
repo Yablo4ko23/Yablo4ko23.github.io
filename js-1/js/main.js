98efbdfbbc97b96a25d7c69031811cd0b5de5803
Yablo4ko23 <56223610+Yablo4ko23@users.noreply.github.com>
2020-02-19 19:17:56 +0200

    Add files via upload

---
+++ js-1/js/main.js
@@ -0,0 +1,90 @@
+//1 номер
+// let str = "Привіт";
+// let num = 123;
+// let flag = true;
+// let txt = "true";
+// console.log(typeof(str));
+// console.log(typeof(num));
+// console.log(typeof(flag));
+// console.log(typeof(txt));
+
+//2 номер
+// let str = "Вітя - ТОП",
+//     flag = false,
+//     obj = {},
+//     number = 1337,
+//     array2 = [1,2,3];
+// const num = 1441,
+//     txt = "false",
+//     array1 = ["hello"],
+//     string = "Купи слона!",
+//     point = true;
+
+//3 номер
+// let myAge = 15,
+//     kolyaAge = 14,
+//     sum = myAge+kolyaAge;
+// alert(sum);
+// let n1 = 108,
+//     n2 = 223,
+//     n3 = 2,
+//     n4 = 5,
+//     result = n1+n2-n3*n4;
+// console.log(result);
+
+//4 номер
+// let a1 = 5+3,
+//     a2 = 5-3,
+//     a3 = 5*3,
+//     a4 = 5/3,
+//     a5 = 5**3;
+// console.log(a1);
+// console.log(a2);
+// console.log(a3);
+// console.log(a4);
+// console.log(a5);
+
+//5 номер
+// let a6 = 5%3,
+//     a7 = 3%5,
+//     a8 = 5+'3',
+//     a9 = '5'-3,
+//     a10 = 75+'кг';
+// alert(a6);
+// alert(a7);
+// alert(a8);
+// alert(a9);
+// alert(a10);
+
+//6 номер
+// let width = 10,
+//     heiht = 23,
+//     s = width*heiht;
+// alert(S);
+
+//7 номер
+// const p = 3.14;
+// let heightC = 10,
+//     dC = 4,
+//     rC = dC/2,
+//     v = p*rC**2*heightC;
+// alert(v);
+
+//8 номер
+// let n = 3,
+//     m = 4,
+//     k = Math.sqrt(3**2 + 4**2);
+// alert(k);
+
+//8 номер(з сайту)
+// let s = 2000,
+//     p = 10,
+//     years = 5,
+//     perepl = ((s/p*5)+s)-s;
+// alert(perepl);
+
+//7 номер(з сайту)
+// let nFib = 12,
+//     el = 12,
+//     fn = Math.pow(nFib, el);
+// alert(fn);
