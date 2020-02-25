a597f8f7599c5048c407b9489a07e6c0f27128d8
Yablo4ko23 <56223610+Yablo4ko23@users.noreply.github.com>
2020-02-25 21:19:50 +0200

    Add files via upload

---
+++ js-2/js/main.js
@@ -0,0 +1,75 @@
+//1
+// let k1='5px',
+//     k2='12djd334',
+//     k3='12.45asdwe12',
+//     k4='qwqweeewq';
+// console.log(Number.parseInt(k1));
+// console.log(Number.parseInt(k2));
+// console.log(Number.parseInt(k3));
+// console.log(Number.parseInt(k4));
+
+//2
+// let k1='5px',
+//     k2='12djd334',
+//     k3='12.45asdwe12',
+//     k4='qwqweeewq';
+// console.log(Number.parseFloat(k1));
+// console.log(Number.parseFloat(k2));
+// console.log(Number.parseFloat(k3));
+// console.log(Number.parseFloat(k4));
+
+//3
+// let random = Math.floor(Math.random() * (10 - 1) + 1);
+// console.log(Math.pow(random, 2));
+
+//4
+// let n1='My',
+//     n2='name',
+//     n3='is',
+//     fullName= `${n1} ${n2} ${n3} Viktor.`;
+// console.log(fullName);
+
+//5
+// let result = 5+5+'5';
+// console.log(typeof result);
+
+//6
+// let num=20;
+// const result = num > 10 && num < 30;
+// console.log(result);
+
+//7
+// let num=40;
+// const result = num < 10 || num > 30;
+// console.log(result);
+
+//8
+// console.log(!true);
+// console.log(!false);
+// console.log(!1);
+// console.log(!0);
+
+//9
+// let a = 1,
+//     b = 4,
+//     c = 4,
+//     d = Math.pow(b, 2) - 4*a*c,
+//     x1 = (-b-Math.sqrt(d))/2*a,
+//     x2 = (-b+Math.sqrt(d))/2*a,
+//     x = -b/(2*a);
+
+//ВІТЯ НЕ БИЙ!!!!! ЦЕ ВСЕ ЩО Я ПРИДУМАВ :-)
+
+// if (d>0) {
+//     console.log('Корені: ', x1, x2);
+// }
+
+// if (d==0) {
+//     console.log('Корінь ', x);
+// }
+
+// if (d<0) {
+//     console.log('Не має коренів!');
+// }
+
+
