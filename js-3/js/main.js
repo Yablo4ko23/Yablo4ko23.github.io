9379ae5cef84a051944df4a21f9529035456e1a8
Yablo4ko23 <56223610+Yablo4ko23@users.noreply.github.com>
2020-03-01 19:33:40 +0200

    Add files via upload

---
+++ js-3/js/main.js
@@ -0,0 +1,76 @@
+//1
+// const data = 1;
+// if (data==1) {
+//     console.log("Сьогодні перше число!");
+// } else {
+//     console.log("Согодні: ", data);
+// }
+
+//2
+// const data = 1;
+// if (data%2==0) {
+//     console.log(data);
+// } else {
+//     console.log("upppss");
+// }
+
+//3
+// let a = 5%2;
+
+// if (a==0) {
+//     console.log(5%2 + " грн");
+// } else {
+//     console.log(a + " євро");
+// }
+
+//4
+// let maks = 55;
+// if (maks>0 && maks<17) {
+//     console.log('kids');
+// } else if (maks>16 && maks<61) {
+//     console.log("adult");
+// } else {
+//     console.log("old");
+// }
+
+//5
+// let a = Math.floor(Math.random() * 2),
+//     b = Math.floor(Math.random() * 2);
+// if (a==b) {
+//     console.log("ФАРТ");
+// } else {
+//     console.log("НЕ ФАРТ");
+// }
+
+//6
+// let name = "Олексій",
+//     surname = "Нізіцький";
+// if (name.length>4 && surname.length>5) {
+//     console.log(name.length + surname.length);
+// } else {
+//     console.log("УПС")
+// }
+
+//7 
+// let number = Math.floor(Math.random() * 5) + 1;
+// if (number==1) {
+//     console.log("Це число один");
+// } else if (number==2) {
+//     console.log("Це число два");
+// } else if (number==3) {
+//     console.log("Це число три");
+// } else if (number==4) {
+//     console.log("Це число чотири");
+// } else {
+//     console.log("Це число п'ять");
+// }
+
+//8
+// let yourAge = prompt("Моєму сусіду зліва: "),
+//     myAge = 15;
+// if (yourAge>12) {
+//     console.log(myAge + Number.parseInt(yourAge));
+// } else {
+//     console.log(yourAge);
+// }
+
