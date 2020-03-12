b4a20f47764ada3ddbbdf37aa44e7100aa4426dd
Yablo4ko23 <56223610+Yablo4ko23@users.noreply.github.com>
2020-03-12 15:25:06 +0200

    Add files via upload

---
+++ js-4/js/main.js
@@ -0,0 +1,63 @@
+//1
+// for (let i=0; i<11; i++) {
+//     console.log(i);
+// }
+
+//2
+// for (let i=1; i<11; i++) {
+//     if (i%2===0) {
+//         console.log("Число парне");
+//     } else {
+//         console.log("Число не парне");
+//     }
+// }
+
+//3
+// let num=20;
+// while (num<29) {
+//     console.log(num);
+//     num++;
+// }
+
+//4
+// let name = prompt("Мого героя звуть: ");
+// while (name.length<6) {
+//     console.log(name);
+//     name = prompt("Мого героя звуть: ");
+// }
+
+//5
+// let mass = [5, 3, 6, 2, 4],
+//     result = 0;
+// for (let i=0; i<mass.length; i++) {
+//     console.log(mass[i]);
+//     result = result+mass[i];
+// } 
+// console.log(result);
+
+//6
+// let result = 0,
+//     mass = [-1, -5, 1, 10, -8, -3, 9, 5];
+// for (let i=0; i<mass.length; i++) {
+//     if (mass[i]>0) {
+//         result+= mass[i];
+//     }
+// }
+// console.log(result);
+
+//7
+// let mass = [1, 2, 3, 4, 5, 6],
+//     str = '';
+// for (let i=0; i<mass.length; i++) {
+//     str = str + mass[i] + '-';
+// }
+// console.log(str);
+
+//8
+// let n=1000,
+//     i=0;
+// for (i; n>50; i++) {
+//     n = n/2;
+// }
+// console.log(i);
+ 
