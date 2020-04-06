cef8ea1508ff2c4600cb89f8f946a496ea488f3d
Yablo4ko23 <56223610+Yablo4ko23@users.noreply.github.com>
2020-04-06 12:33:28 +0300

    Add files via upload

---
+++ js-8/js/main.js
@@ -0,0 +1,51 @@
+//1
+// let inputs = document.querySelectorAll(".text-input"),
+//     firstInput = document.querySelector(".first-input");
+
+//2
+// let title = document.querySelector(".title-head");
+// title.textContent = 'i love my mom';
+
+//3
+// let firstP = document.querySelector(".more-text"),
+//     secondP = document.querySelector(".new-text");
+// firstP.classList.add('active');
+// secondP.classList.remove('new-text');
+
+//4
+// let firstInput = document.querySelector(".first-input"),
+//     result = document.querySelector(".more-text");
+// function sum () {
+//     result.textContent = firstInput.value*4;
+// }
+// sum();
+
+//5
+// let inputs = document.getElementsByClassName('input');
+//     objGlobal = {
+//         age : inputs[0].value,
+//         surname : inputs[1].value,
+//         firstName : inputs[2].value,
+//         secondName : inputs[3].value
+//     };
+// console.log(objGlobal);
+
+//6
+// let img = document.querySelector(".first-img");
+// img.src = "./img/second.jpg";
+
+//7
+// let thirdImg = document.querySelector(".beautiful");
+// if (thirdImg.classList.contains('beautiful')) {
+//     thirdImg.src = "./img/four.jpg";
+// }
+
+//8
+// let checkbox = document.querySelectorAll(".checkbox");
+// for (let i=0; i<checkbox.length; i++) {
+//     if (checkbox[i].checked === true) {
+//         checkbox[i].checked = false;
+//     } else {
+//         checkbox[i].checked = true;
+//     }
+// }
