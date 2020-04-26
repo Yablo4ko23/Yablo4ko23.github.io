761c4d28c64657a127e5461ed24ceb66261c67ce
Yablo4ko23 <56223610+Yablo4ko23@users.noreply.github.com>
2020-04-26 18:17:36 +0300

    Add files via upload

---
+++ js-10/js/main.js
@@ -0,0 +1,48 @@
+//1
+// let arr = [1,2,3,4,5,6];
+// arr.forEach((item) => console.log(item+1));
+
+//2
+// let btn = document.querySelector(".sbm"),
+//     inputs = document.querySelectorAll(".foreach");
+// btn.addEventListener("click", function() {
+//     inputs.forEach((item) => console.log(item.value));
+// });
+
+//3
+// let inputs = document.querySelectorAll("input");
+// inputs.forEach(function(item){
+//     item.addEventListener("click", function(){
+//         console.log(item);
+//     });
+// });
+
+//4
+// let inputs = document.querySelectorAll(".block");
+// inputs.forEach(function(item, i){
+//     item.addEventListener("click", function(){
+//         if (item.classList.contains("active")){
+//             item.classList.remove("active");
+//         } else {
+//             item.classList.add("active");
+//         }
+//         i++;
+//         console.log(i);
+//     });
+// });
+
+//5
+// let inputs = document.querySelectorAll(".block");
+// inputs.forEach(function(item){
+//     item.addEventListener("click", function(){
+//         console.log(item);
+//     });
+// });
+
+//6
+// let inputs = document.querySelectorAll("input");
+// inputs.forEach(function(item){
+//     if (item.classList.contains("ch")){
+//         item.checked = true;
+//     }
+// });
